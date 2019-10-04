bd87c24d54b62fb31fff3a6d7507a6c2e0f1a8c8
Yusuf Olokoba <olokobayusuf@gmail.com>
2019-10-04 15:36:03 -0400

    Update NatCamSource.cs

--- Assets/NatCamWithOpenCVForUnityExample/Scripts/NatCamSource.cs
+++ Assets/NatCamWithOpenCVForUnityExample/Scripts/NatCamSource.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using System;
-using System.Runtime.InteropServices;
 using NatCam;
 using OpenCVForUnity.CoreModule;
 using OpenCVForUnity.UnityUtils;
@@ -12,98 +11,99 @@ namespace NatCamWithOpenCVForUnityExample
     {
 
         #region --Op vars--
-
-        private DeviceCamera camera;
         private Action startCallback, frameCallback;
-        private byte[] sourceBuffer;
         private int requestedWidth, requestedHeight, requestedFramerate;
         private int cameraIndex;
-
+        private Texture2D previewTexture;
         #endregion
 
 
         #region --Client API--
 
-        public int width { get; private set; }
+        public int width {
+            get; private set;
+        }
 
-        public int height { get; private set; }
+        public int height {
+            get; private set;
+        }
 
-        public bool isRunning { get { return camera.IsRunning; } }
+        public bool isRunning {
+            get { return cameraDevice.IsRunning; }
+        }
 
-        public Texture preview { get; private set; }
+        public Texture preview {
+            get { return previewTexture; }
+        }
 
-        public DeviceCamera activeCamera { get { return camera; } }
+        public CameraDevice cameraDevice {
+            get; private set;
+        }
 
         public NatCamSource (int width, int height, int framerate, bool front)
         {
             this.requestedWidth = width;
             this.requestedHeight = height;
             this.requestedFramerate = framerate;
-
-            for (; cameraIndex < DeviceCamera.Cameras.Length; cameraIndex++)
-                if (DeviceCamera.Cameras [cameraIndex].IsFrontFacing == front)
+            
+            var cameraDevices = CameraDevice.GetDevices();
+            for (; cameraIndex < cameraDevices.Length; cameraIndex++)
+                if (cameraDevices[cameraIndex].IsFrontFacing == front)
                     break;
 
-            if (cameraIndex == DeviceCamera.Cameras.Length) {
+            if (cameraIndex == cameraDevices.Length) {
                 Debug.LogError ("Camera is null. Consider using " + (front ? "rear" : "front") + " camera.");
                 return;
             }
 
-            camera = DeviceCamera.Cameras [cameraIndex];
-            camera.PreviewResolution = new Vector2Int (width, height);
-            camera.Framerate = framerate;
+            cameraDevice = cameraDevices[cameraIndex];
+            cameraDevice.PreviewResolution = new Vector2Int(width, height);
+            cameraDevice.Framerate = framerate;
         }
 
         public void Dispose ()
         {
-            if (camera.IsRunning)
-                camera.StopPreview ();
-            sourceBuffer = null;
-            preview = null;
+            if (cameraDevice.IsRunning)
+                cameraDevice.StopPreview();
+            previewTexture = null;
         }
 
         public void StartPreview (Action startCallback, Action frameCallback)
         {
             this.startCallback = startCallback;
             this.frameCallback = frameCallback;
-            camera.StartPreview (
-                (Texture preview) => {
+            cameraDevice.StartPreview(
+                (Texture2D preview) => {
+                    previewTexture = preview;
                     width = preview.width;
                     height = preview.height;
-                    this.preview = preview;
-                    sourceBuffer = new byte[width * height * 4];
                     startCallback ();
                 },
-                frameCallback
+                timestamp => frameCallback()
             );
         }
 
         public void CaptureFrame (Mat matrix)
         {
-            camera.CaptureFrame (sourceBuffer);
-            Utils.copyToMat (sourceBuffer, matrix);
-            Core.flip (matrix, matrix, 0);
+            Utils.texture2DToMat (previewTexture, matrix);
+            //Core.flip (matrix, matrix, 0); // Shouldn't be necessary
         }
 
         public void CaptureFrame (Color32[] pixelBuffer)
         {
-            camera.CaptureFrame (sourceBuffer);
-
-            GCHandle pin = GCHandle.Alloc (pixelBuffer, GCHandleType.Pinned);
-            Marshal.Copy (sourceBuffer, 0, pin.AddrOfPinnedObject (), sourceBuffer.Length);
-            pin.Free ();
+            previewTexture.GetRawTextureData<Color32>().CopyTo(pixelBuffer);
         }
 
         public void SwitchCamera ()
         {
-            if (camera.IsRunning)
-                camera.StopPreview ();
-
-            cameraIndex = ++cameraIndex % WebCamTexture.devices.Length;
-            camera = DeviceCamera.Cameras [cameraIndex];
-            camera.PreviewResolution = new Vector2Int (requestedWidth, requestedHeight);
-            camera.Framerate = requestedFramerate;
-            StartPreview (startCallback, frameCallback);
+            if (cameraDevice.IsRunning)
+                cameraDevice.StopPreview ();
+            var cameraDevices = CameraDevice.GetDevices();
+            cameraIndex = ++cameraIndex % cameraDevices.Length;
+            cameraDevice = cameraDevice[cameraIndex];
+            cameraDevice.PreviewResolution = new Vector2Int(requestedWidth, requestedHeight);
+            cameraDevice.Framerate = requestedFramerate;
+            StartPreview(startCallback, frameCallback);
         }
 
         #endregion
